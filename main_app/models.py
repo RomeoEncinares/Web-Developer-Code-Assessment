a8e347af0ae102db7159d295d83c440d420e9d90
Romeo Encinares <romeoncrs@gmail.com>
2023-02-09 16:04:48 +0800

    Change Articles OneToOne field to ForeignKey

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
 
