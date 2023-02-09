3318fe4d02d09eceff3765b48878fbbda44049c8
Romeo Encinares <romeoncrs@gmail.com>
2023-02-09 14:53:51 +0800

    Add login and home in urlpatterns

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
-from .views import index, register, login
+from .views import index, register, login, home
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', register, name='register_user'),
     path('login/', login, name='login_user'),
+    path('home/', home, name='home_user'),
 ]
